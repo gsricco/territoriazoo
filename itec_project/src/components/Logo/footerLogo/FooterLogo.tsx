7cbba465af7e0e47e86278411c21038da67d3aae
Natalia <zakrevskaya.97@inbox.ru>
2022-09-14 14:22:04 +0300

    fix cursor for FooterLogo.tsx and header logo

--- itec_project/src/components/Logo/footerLogo/FooterLogo.tsx
+++ itec_project/src/components/Logo/footerLogo/FooterLogo.tsx
@@ -1,14 +1,20 @@
 import React, { ReactElement } from 'react';
 import logo from '../../../Images/footerLogo.svg';
 import style from './FooterLogo.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { routesPathsEnum } from '../../../routes/enums';
 
-const FooterLogo = React.memo((): ReactElement => {
+const FooterLogo = React.memo( (): ReactElement => {
+  const navigate = useNavigate();
+  const onLogoClick = () => {
+    navigate( routesPathsEnum.MAIN );
+  };
   return (
-    <div className={ style.logoBlock }>
-      <img src={ logo } loading={'lazy'} alt={ 'logo' }/>
+    <div className={ style.logoBlock } onclick={ onLogoClick }>
+      <img src={ logo } loading={ 'lazy' } alt={ 'logo' }/>
       <p>Территория <span>ZOO</span></p>
     </div>
   );
-});
+} );
 
 export default FooterLogo;
