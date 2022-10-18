e17d2966a9da41f23bd3eeccd017ade1105588fc
korneyka3000 <korneyburau@gmail.com>
2022-10-18 20:40:14 +0300

    re_path for smart-selects

--- ZOO/ZOO/urls.py
+++ ZOO/ZOO/urls.py
@@ -1,8 +1,8 @@
-from compose.config.interpolation import re_path
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
 
