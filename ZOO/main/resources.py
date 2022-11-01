3f2c3406bcc584cd8b59c4c9157c237a60e8ba36
korneyka3000 <korneyburau@gmail.com>
2022-11-01 12:29:08 +0300

    resources.py changed

--- ZOO/main/resources.py
+++ ZOO/main/resources.py
@@ -1,115 +1,230 @@
 from django.db.models import QuerySet
-from import_export import resources, fields
+from import_export import fields, resources
 from import_export.fields import Field
-from import_export.widgets import ForeignKeyWidget, ManyToManyWidget, CharWidget, DecimalWidget, IntegerWidget, \
-    BooleanWidget
-from .models import Product, Animal, Category, Brand, ProductOptions, Units
+from import_export.instance_loaders import CachedInstanceLoader
+from import_export.widgets import (
+    BooleanWidget,
+    CharWidget,
+    DecimalWidget,
+    ForeignKeyWidget,
+    IntegerWidget,
+    ManyToManyWidget,
+)
+
+from .models import Animal, Brand, Category, Product, ProductOptions, SubCategory, Units
 
 
-class ForeignKeyWidgetWithCreation(ForeignKeyWidget):
-    def __init__(self, model, field="pk", create=False, **kwargs):
-        self.model = model
-        self.field = field
-        self.create = create
-        super(ForeignKeyWidgetWithCreation, self).__init__(model, field=field, **kwargs)
+# class ForeignKeyWidgetWithCreation(ForeignKeyWidget):
+#     def __init__(self, model, field="pk", create=False, **kwargs):
+#         self.model = model
+#         self.field = field
+#         self.create = create
+#         super(ForeignKeyWidgetWithCreation, self).__init__(model, field=field, **kwargs)
+#
+#     def clean(self, value, **kwargs):
+#         if not value:
+#             return None
+#         if self.create:
+#             self.model.objects.get_or_create(**{self.field: value})
+#         val = super(ForeignKeyWidgetWithCreation, self).clean(value, **kwargs)
+#         return self.model.objects.get(**{self.field: val}) if val else None
+#
+#
+# class ManyToManyWidgetWithCreation(ManyToManyWidget):
+#     def __init__(self, model, field="pk", create=False, **kwargs):
+#         self.model = model
+#         self.field = field
+#         self.create = create
+#         super(ManyToManyWidgetWithCreation, self).__init__(model, field=field, **kwargs)
+#
+#     def clean(self, value, **kwargs):
+#         if not value:
+#             return self.model.objects.none()
+#         cleaned_value: QuerySet = super(ManyToManyWidgetWithCreation, self).clean(
+#             value, **kwargs
+#         )
+#         object_list = value.split(self.separator)
+#         if len(cleaned_value.all()) == len(object_list):
+#             return cleaned_value
+#         if self.create:
+#             for object_value in object_list:
+#                 _instance, _new = self.model.objects.get_or_create(
+#                     **{self.field: object_value}
+#                 )
+#         model_objects = self.model.objects.filter(**{f"{self.field}__in": object_list})
+#         return model_objects
 
-    def clean(self, value, **kwargs):
-        if not value:
-            return None
-        if self.create:
-            self.model.objects.get_or_create(**{self.field: value})
-        val = super(ForeignKeyWidgetWithCreation, self).clean(value, **kwargs)
-        return self.model.objects.get(**{self.field: val}) if val else None
 
-
-class ManyToManyWidgetWithCreation(ManyToManyWidget):
-    def __init__(self, model, field="pk", create=False, **kwargs):
-        self.model = model
-        self.field = field
-        self.create = create
-        super(ManyToManyWidgetWithCreation, self).__init__(model, field=field, **kwargs)
-
-    def clean(self, value, **kwargs):
-        if not value:
-            return self.model.objects.none()
-        cleaned_value: QuerySet = super(ManyToManyWidgetWithCreation, self).clean(value, **kwargs)
-        object_list = value.split(self.separator)
-        if len(cleaned_value.all()) == len(object_list):
-            return cleaned_value
-        if self.create:
-            for object_value in object_list:
-                _instance, _new = self.model.objects.get_or_create(**{self.field: object_value})
-        model_objects = self.model.objects.filter(**{f"{self.field}__in": object_list})
-        return model_objects
-
-
-class ProductOptionsAdminResource(resources.ModelResource):
-    article_number = Field(column_name='Артикул', attribute='article_number')
-    product = fields.Field(column_name='Название продукта', attribute='product',
-                           widget=ForeignKeyWidgetWithCreation(Product, field='name', create=True))
-    price = Field(column_name='Цена', attribute='price')
-    size = Field(column_name='Объём', attribute='size')
-    stock_balance = Field(column_name='Остаток на складе', attribute='stock_balance')
-    units = fields.Field(column_name='Единицы измерения', attribute='units',
-                         widget=ForeignKeyWidgetWithCreation(Units, field='unit_name', create=True))
-    is_active = Field(column_name='Активно', attribute='is_active', default=True)
-    partial = Field(column_name='На развес', attribute='partial', default=False)
+class ProductOptionsResource(resources.ModelResource):
+    article_number = fields.Field(column_name="Артикул", attribute="article_number")
+    weight = fields.Field(
+        column_name="Вес",
+    )
+    volume = fields.Field(
+        column_name="Объем",
+    )
+    length = fields.Field(
+        column_name="Длина",
+    )
+    price = fields.Field(column_name="Цена", attribute="price")
+    units = fields.Field(column_name="Ед.изм.", attribute="units1")
+    # widget=ForeignKeyWidgetWithCreation(Units, field='unit_name', create=True))
+    product = fields.Field(column_name="Номенклатура", attribute="product1")
+    stock_balance = fields.Field(column_name="Остаток", attribute="stock_balance")
+    brand = fields.Field(column_name="Бренд", attribute="brand")
+    animal = fields.Field(column_name="Животное", attribute="animal")
+    category = fields.Field(column_name="Категория товаров", attribute="category")
+    sub_category = fields.Field(
+        column_name="Подкатегория товаров", attribute="sub_category"
+    )
+    is_active = fields.Field()
 
     class Meta:
         model = ProductOptions
-        widgets = {
-            'article_number': CharWidget,
-            'price': DecimalWidget,
-            'size': IntegerWidget,
-            'stock_balance': IntegerWidget,
-            'is_active': BooleanWidget,
-            'partial': BooleanWidget
-        }
-        fields = ('article_number', 'product', 'price', 'size', 'stock_balance', 'units', 'is_active', 'partial')
-        export_order = ('article_number', 'product', 'price', 'size', 'stock_balance', 'units', 'is_active', 'partial')
-        import_id_fields = ('article_number',)  # поля для определения идентификатора
-        exclude = ('id', 'date_created', 'date_updated',)  # поля для исключения
+        fields = (
+            "article_number",
+            "weight",
+            "volume",
+            "length",
+            "price",
+            "unit",
+            "product",
+            "brand",
+            "animal",
+            "category",
+            "sub_category",
+            "stock_balance",
+        )
+        skip_unchanged = True
+        report_skipped = True
+        use_transactions = True
+        import_id_fields = ("article_number",)
+        exclude = ("id",)
+        # skip_admin_log = True
+        batch_size = 100
+        use_bulk = True
+        # skip_html_diff = True
+        instance_loader_class = CachedInstanceLoader
 
     def before_import_row(self, row, row_number=None, **kwargs):
-        if row['На развес'] == 'True' or row['На развес'] == '1':
-            row['Объём'] = '1000'
-            row['Единицы измерения'] = 'грамм'
+        self.brand = row["Бренд"]
+        self.animal = row["Животное"]
+        self.category = row["Категория товаров"]
+        self.sub_category = row["Подкатегория товаров"]
+        self.product = row["Номенклатура"]
+        # self.units = row.pop('Ед.изм.')
+        self.units = row["Ед.изм."]
+        self.stock_balance = row["Остаток"]
+        # weight = row.pop('Вес')
+        # length = row.pop('Длина')
+        # volume = row.pop('Объем')
+        weight = row["Вес"]
+        length = row["Длина"]
+        volume = row["Объем"]
+        if weight is not None:
+            self.size = weight
+        elif length is not None:
+            self.size = length
+        elif volume is not None:
+            self.size = volume
+        if row["Ед.изм."] == "кг":
+            row["Вес"] = 1000
+            row["Ед.изм."] = "грамм"
+            row["Остаток"] = row["Остаток"] * 1000
 
+    def after_import_instance(self, instance, new, row_number=None, **kwargs):
 
-class ProductAdminResource(resources.ModelResource):
-    name = Field(column_name='Название товара', attribute='name')
-    description = Field(column_name='Описание товара', attribute='description')
-    features = Field(column_name='Ключевые особенности', attribute='features')
-    composition = Field(column_name='Состав', attribute='composition')
-    additives = Field(column_name='Пищевые добавки', attribute='additives')
-    analysis = Field(column_name='Гарантированный анализ', attribute='analysis')
-    is_active = Field(column_name='Активен', attribute='is_active', default=False)
-    animal = fields.Field(column_name='Животные', attribute='animal',
-                          widget=ManyToManyWidgetWithCreation(Animal, field='name', separator=', ', create=True))
-    brand = fields.Field(column_name='Бренды', attribute='brand',
-                         widget=ForeignKeyWidgetWithCreation(Brand, field='name', create=True))
-    category = fields.Field(column_name='Категории', attribute='category',
-                            widget=ForeignKeyWidgetWithCreation(Category, field='name', create=True))
-
-    class Meta:
-        model = Product
-        widgets = {
-            'name': CharWidget,
-            'description': CharWidget,
-            'features': CharWidget,
-            'composition': CharWidget,
-            'additives': CharWidget,
-            'analysis': CharWidget,
-            'is_active': BooleanWidget,
-        }
-        fields = (
-            'name', 'description', 'features', 'composition', 'additives', 'analysis', 'animal', 'brand', 'category',
-            'is_active'
+        if "/" in self.animal:
+            list_animal = self.animal.split("/")
+            animals_list = []
+            categories_list = []
+            subcat_list = []
+            for new_animal in list_animal:
+                animal, created = Animal.objects.get_or_create(
+                    name=new_animal.capitalize()
+                )
+                animals_list.append(animal.id)
+                category, created = Category.objects.get_or_create(
+                    name=self.category, animal=animal
+                )
+                categories_list.append(category.id)
+                sub_category, created = SubCategory.objects.get_or_create(
+                    name=self.sub_category, category=category
+                )
+                subcat_list.append(sub_category.id)
+                brand, created = Brand.objects.get_or_create(name=self.brand)
+                product, created = Product.objects.get_or_create(
+                    name=self.product,
+                    brand=brand,
+                )
+            product.animal.set(animals_list)
+            product.category.set(categories_list)
+            product.subcategory.set(subcat_list)
+            product.animal_ids = animals_list
+            product.category_ids = categories_list
+            product.subcategory_ids = subcat_list
+        else:
+            animal, created = Animal.objects.get_or_create(
+                name=self.animal.capitalize()
+            )
+            category, created = Category.objects.get_or_create(
+                name=self.category, animal=animal
+            )
+            sub_category, created = SubCategory.objects.get_or_create(
+                name=self.sub_category, category=category
+            )
+            brand, created = Brand.objects.get_or_create(name=self.brand)
+            product, created = Product.objects.get_or_create(
+                name=self.product, brand=brand
+            )
+            product.animal.set((animal,))
+            product.category.set((category,))
+            product.subcategory.set((sub_category,))
+            product.animal_ids = [animal.id]
+            product.category_ids = [category.id]
+            product.subcategory_ids = [sub_category.id]
+        product.save()
+        unit_instance, is_created_unit = Units.objects.get_or_create(
+            unit_name=self.units
         )
-        # порядок экспорта полей
-        export_order = (
-            'name', 'description', 'features', 'composition', 'additives', 'analysis', 'animal', 'brand', 'category',
-            'is_active'
+        if self.stock_balance <= 0:
+            instance.is_active = False
+        if self.units == "кг":
+            unit_for_partial, created_unit = Units.objects.get_or_create(
+                unit_name="грамм"
+            )
+            instance.partial = 1
+            instance.units = unit_for_partial
+            instance.size = 1000
+            instance.stock_balance = self.stock_balance * 1000
+        else:
+            # unit_for_instance, created_unit = Units.objects.get_or_create(unit_name=self.units)
+            # print(unit_instance, 'in else statement')
+
+            instance.units = unit_instance
+            instance.size = self.size
+        instance.product = product
+
+    def bulk_create(self, using_transactions, dry_run, raise_errors, batch_size=None):
+        super().bulk_create(using_transactions, dry_run, raise_errors, batch_size=None)
+
+    def get_bulk_update_fields(self):
+        not_bulk_fields = (
+            "article_number",
+            "weight",
+            "length",
+            "volume",
+            "brand",
+            "animal",
+            "sub_category",
+            "category",
         )
-        import_id_fields = ('name',)  # поля для определения идентификатора
-        exclude = ('id', 'date_added', 'popular')  # поля для исключения
+        return [f for f in self.fields if f not in not_bulk_fields]
+
+    def bulk_update(self, using_transactions, dry_run, raise_errors, batch_size=None):
+        for instance in self.update_instances:
+            if instance.stock_balance <= 0:
+                instance.is_active = False
+            else:
+                instance.is_active = True
+        super().bulk_update(using_transactions, dry_run, raise_errors, batch_size=None)
