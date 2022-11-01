1d8f96e1748e520aa2832c0f6a61610aaf2f542e
korneyka3000 <korneyburau@gmail.com>
2022-11-01 13:15:29 +0300

    settings.py changed

--- ZOO/ZOO/settings.py
+++ ZOO/ZOO/settings.py
@@ -140,7 +140,7 @@ ADMIN_REORDER = (
         'app': 'main',
         'label': 'Скидки',
         'models': [
-            {'model': 'main.DiscountProductOption', 'label': 'Скидки по варианту фасовки'},
+            {'model': 'main.DiscountByProductOption', 'label': 'Скидки по варианту фасовки'},
             {'model': 'main.DiscountBySubCategory', 'label': 'Скидка по подкатегории'},
             {'model': 'main.DiscountByDay', 'label': 'Скидка по дням недели'},
             {'model': 'main.WeekDays', 'label': 'Дни недели'},
