0b02c9d2106034bb5ac455de4f90ce763afc28ba
korneyka3000 <korneyburau@gmail.com>
2022-11-01 12:34:14 +0300

    urls.py and validators.py changed

--- ZOO/main/urls.py
+++ ZOO/main/urls.py
@@ -1,25 +1,28 @@
-from rest_framework.routers import SimpleRouter, DefaultRouter
 from django.urls import path
-from main.views import (ProductViewSet, BrandViewSet, AnimalViewSet, CategoryViewSet,
-                        ArticleViewSet, CommentsView, InfoShopView, OrderViewSet,
-                        DiscountByDayViewSet,)
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+from rest_framework.routers import DefaultRouter, SimpleRouter
+
+from .views import (
+    AnimalViewSet,
+    ArticleViewSet,
+    BrandViewSet,
+    CategoryViewSet,
+    CommentsView,
+    DiscountByDayViewSet,
+    InfoShopView,
+    OrderViewSet,
+    ProductViewSet,
 )
+
 router = DefaultRouter()
 
-router.register(r'products', ProductViewSet, basename='products')
-router.register(r'brands', BrandViewSet, basename='brands')
-router.register(r'animals', AnimalViewSet, basename='animals')
-router.register(r'categories', CategoryViewSet, basename='categories')
-router.register(r'articles', ArticleViewSet, basename='articles')
-router.register(r'comments', CommentsView, basename='comments')
-router.register(r'order', OrderViewSet, basename='order')
-router.register(r'shop-info', InfoShopView, basename='shop_info')
-router.register(r'discounts', DiscountByDayViewSet, basename='discounts')
-urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+router.register(r"products", ProductViewSet, basename="products")
+router.register(r"brands", BrandViewSet, basename="brands")
+router.register(r"animals", AnimalViewSet, basename="animals")
+router.register(r"categories", CategoryViewSet, basename="categories")
+router.register(r"articles", ArticleViewSet, basename="articles")
+router.register(r"comments", CommentsView, basename="comments")
+router.register(r"order", OrderViewSet, basename="order")
+router.register(r"shop-info", InfoShopView, basename="shop_info")
+router.register(r"discounts", DiscountByDayViewSet, basename="discounts")
+urlpatterns = []
 urlpatterns += router.urls
--- ZOO/main/validators.py
+++ ZOO/main/validators.py
@@ -1,19 +1,22 @@
 import re
+
 from django.core.exceptions import ValidationError
 
 
 def phone_validator(phone_number):
-    regular = r'(\+375)?(?:33|44|25|29)?([0-9]{7})'
+    regular = r"(\+375)?(?:33|44|25|29)?([0-9]{7})"
     if re.fullmatch(regular, phone_number):
         return phone_number
     else:
-        raise ValidationError('Неправильный номер телефона')
+        raise ValidationError("Неправильный номер телефона")
 
 
 def name_validator(name):
-    regular = r'^([А-Яа-я]{1}[а-яё]{0,15}[\s]{0,3}[А-Яа-я]{0,1}[а-яё]{0,15}|' \
-              r'[A-Za-z]{1}[a-z]{0,15}[\s]{0,3}[A-Za-z]{0,1}[a-z]{0,15})$'
+    regular = (
+        r"^([А-Яа-я]{1}[а-яё]{1,30}[\s]{0,3}[А-Яа-я]{1}[а-яё]{1,30}|"
+        r"[A-Za-z]{1}[a-z]{1,30}[\s]{0,3}[A-Za-z]{1}[a-z]{1,30})$"
+    )
     if re.fullmatch(regular, name):
         return name
     else:
-        raise ValidationError('Неверное имя!!!')
+        raise ValidationError("Неверное имя!!!")
