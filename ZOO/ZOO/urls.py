b8c23ee53bfadbafb87b7649d925e6822ca105d1
korneyka3000 <korneyburau@gmail.com>
2022-11-03 17:18:18 +0300

    jwt

--- ZOO/ZOO/urls.py
+++ ZOO/ZOO/urls.py
@@ -1,10 +1,13 @@
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("api_schema/", get_schema_view(title="API SCHEMA", description="Guide to Territory_Zoo API"),
@@ -15,6 +18,8 @@ urlpatterns = [
     re_path(r'^chaining/', include('smart_selects.urls')),
     path('api/', include('main.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 if settings.DEBUG:
