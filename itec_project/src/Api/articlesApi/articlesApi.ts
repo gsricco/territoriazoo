becf88ad38eada65fff612f5e61c91f7941b38b2
Natalia <zakrevskaya.97@inbox.ru>
2022-11-09 14:21:46 +0300

    fix query params for articles request

--- itec_project/src/Api/articlesApi/articlesApi.ts
+++ itec_project/src/Api/articlesApi/articlesApi.ts
@@ -1,10 +1,13 @@
 import { instance } from '../config';
 import { ARTICLES_URL } from './constants';
-import { ResBrandType } from '../../types';
 
 export const articlesAPI = {
-  async setArticles() {
-    return await instance.get<Array<ResBrandType>>( ARTICLES_URL );
+  async setArticles( animal?: number | null ) {
+    return await instance.get( ARTICLES_URL, {
+      params: {
+        animal: animal
+      }
+    } );
   },
   async getArticle( id: number ) {
     return await instance.get( `${ ARTICLES_URL }${ id }` );
