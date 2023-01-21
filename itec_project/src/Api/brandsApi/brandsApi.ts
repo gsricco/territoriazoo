3da641bc2e8166f1d00e3ecf86ffdfd4ec273026
korneyka3000 <korneyburau@gmail.com>
2023-01-21 18:18:45 +0300

    refactor of FRONTEND, js developer fullstack

--- itec_project/src/Api/brandsApi/brandsApi.ts
+++ itec_project/src/Api/brandsApi/brandsApi.ts
@@ -3,9 +3,12 @@ import {BRANDS_URL} from './constants';
 import {GetBrandsType, ResBrandType} from '../../types';
 
 export const brandsAPI = {
-    async setBrands({animalId = 6, categoryId}: GetBrandsType) {
+    async setBrands({animalId, categoryId}: GetBrandsType) {
         if (categoryId) {
             return await instance.get<Array<ResBrandType>>(`${BRANDS_URL}?animal=${animalId}&category=${categoryId}`);
-        } else return await instance.get<Array<ResBrandType>>(`${BRANDS_URL}?animal=${animalId}`);
+        } else if(animalId) {
+            return await instance.get<Array<ResBrandType>>(`${BRANDS_URL}?animal=${animalId}`);
+        }
+        else return await instance.get<Array<ResBrandType>>(`${BRANDS_URL}`);
     },
 }
